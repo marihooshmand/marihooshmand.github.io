a5b816a226a9b8381c7b8c897e2d583b84f3509a
marihooshmand <109873772+marihooshmand@users.noreply.github.com>
2023-05-02 12:27:37 +0330

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,25 @@
 let display=document.querySelector('#display');
+let btnclear=document.querySelector('#btn-clear');
+let btnclearlastaction=document.querySelector('#btn-clear-last-action');
+let btnDiv=document.querySelector('#btnDiv');
+let btnmul=document.querySelector('#btn-mul');
+let btnminus=document.querySelector('#btn-minus');
+let btnplus=document.querySelector('#btn-plus');
+let btnpn=document.querySelector('#btn-pn');
+let btnpoint=document.querySelector('#btn-point');
+let btnquals=document.querySelector('#btn-quals');
+
+
+let btnumbers=document.querySelectorAll('.btn-number');
+
+
+btnclear.addEventListener('clic',(e)=>{
+
+});
+
+console.log(numbers);
+
+//btnnumbers.array.forEach(element => {
+    
+//});
+
